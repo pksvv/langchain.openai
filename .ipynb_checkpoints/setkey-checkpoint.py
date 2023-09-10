ed83b39d4d7a502b4c268888aecc646c99a58f15
Vipul <gaurvipul@gmail.com>
2023-09-11 00:07:49 +0530

    Update setkey-checkpoint.py

--- .ipynb_checkpoints/setkey-checkpoint.py
+++ .ipynb_checkpoints/setkey-checkpoint.py
@@ -1,4 +1,1 @@
-import os
-OPENAI_API_KEY='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'
-os.environ['OPENAI_API_KEY']='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'
-print('OPENAI_API_KEY set !')
+

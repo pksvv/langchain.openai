bcae1e59ea7bdb88030816a789e87af9f4c7a29f
Renu Gaur <renugaur@Vipuls-MacBook-Pro.local>
2023-09-11 05:29:34 +0530

    5

---
+++ .ipynb_checkpoints/setkey-checkpoint.py
@@ -0,0 +1,4 @@
+import os
+OPENAI_API_KEY='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'
+os.environ['OPENAI_API_KEY']='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'
+print('OPENAI_API_KEY set !')
